cde37cd095e8bf4e98c366a6c8f8ce4b56ac75e8
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2022-01-25 00:40:17 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,7 +16,7 @@ const Home = ({ darkMode }) => {
     
             <AttentionSeeker effect="pulse">  
                 <Row>  
-   <Particles />
+
                     <Col lg={12} xs={12}>
      
                         <h2 className="display-3 hello" align="center"><b>  Gaussian Curvature </b> </h2>
@@ -53,7 +53,7 @@ const Home = ({ darkMode }) => {
                 <Card.Header><div class= "lead" >About Us</div></Card.Header>
                                 <Card.Body>
                                    
-                                    <Card.Text align="left" class={darkMode? "text-secondary" : "text-light"}> <Particles />The Gaussian Curvature team consists of Math Olympiad students who enjoy Mathematics to a great extent. We aim to help others discover their love for Mathematics beyond the school syllabus, and to develop their passion for problem-solving. Through our training materials and contests, we hope to help those who have a keen interest in Math Olympiad excel.
+                                    <Card.Text align="left" class={darkMode? "text-secondary" : "text-light"}> The Gaussian Curvature team consists of Math Olympiad students who enjoy Mathematics to a great extent. We aim to help others discover their love for Mathematics beyond the school syllabus, and to develop their passion for problem-solving. Through our training materials and contests, we hope to help those who have a keen interest in Math Olympiad excel.
 <br></br>
 <br></br>   <Flex>
        {darkMode? <MagicalButton href="">Details</MagicalButton> : <Mdark href="">About Us</Mdark> }
