b85cc5f7e2db1c345077c5d84b9c50447146023b
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2022-01-24 15:27:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -106,7 +106,7 @@ function App() {
 </Route>
 <Route path="/mmo2020">
     <Navbar expand="lg" className="navbar" data-theme={darkMode ? "light" : "light"}>
-            <Navbar.Brand href="#home" className="navname"> </Navbar.Brand>
+            <Navbar.Brand href="#home" className="navname"><img src={darkMode ? helloDark : helloLight} alt="hello" className="hello-img" width="100" /> </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
@@ -130,7 +130,7 @@ function App() {
 </Route>
 <Route path="/team">
     <Navbar expand="lg" className="navbar" data-theme={darkMode ? "dark" : "light"}>
-            <Navbar.Brand href="#home" className="navname">  <img src={darkMode ? helloDark : helloLight} alt="hello" className="hello-img" width="150" /></Navbar.Brand>
+            <Navbar.Brand href="#home" className="navname">  
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
@@ -154,7 +154,7 @@ function App() {
 </Route>
 <Route path="/contact">
     <Navbar expand="lg" className="navbar" data-theme={darkMode ? "dark" : "light"}>
-            <Navbar.Brand href="#home" className="navname">  <img src={darkMode ? helloDark : helloLight} alt="hello" className="hello-img" width="150" /></Navbar.Brand>
+            <Navbar.Brand href="#home" className="navname">  <img src={darkMode ? helloDark : helloLight} alt="hello" className="hello-img" width="100" /></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
