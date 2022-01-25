e9ddba6e7357d41b25b0428665f24c27dc68d6e2
gaussiancurvolympiads <79222966+gaussiancurvolympiads@users.noreply.github.com>
2022-01-25 23:34:57 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -29,7 +29,7 @@ const Skills = ({ darkMode }) => {
                                      <div class={darkMode ? "lead" : "lead"}>GIMO 2021 Day 1</div>      <br></br>       
                                     <Card.Text align="left">
                                                  
-                                       <a  href="https://mail-attachment.googleusercontent.com/attachment/u/1/?ui=2&ik=e3eea35a15&attid=0.2&permmsgid=msg-f:1701998537667956915&th=179eb6a705eed0b3&view=att&disp=inline&realattid=f_kpnywooe1&saddbat=ANGjdJ9f4zbL0QVIaIjFmH_nNkNl8NBFkrdqv0sLnq3MU_EvkTt8ZL1G4nSJr8N0G44xuYERq6v4y6990xTwLg9uEHXk79uc-_Q-G7cVz-WbQFoVSvqgAQnUEtv1RayiViXe79WxdLQ1wEYJNuOD_582PrcnX5RZ0MiizRbGjFroybCvKVneKxpjE1OLIxHSPltDxkPvmieutZIHTVUUczAj8q8HeGph7CbFjMStjrGX9w1bhRoRCk8Zk3drkmM88JSA-T9w3ZvLHmiPjw0ZpTeJqJHIjghX3tI1b1sMpPodv0WbSh2pndDMNIQg6HfUxWSrSzIrAKtxY9I8PdTJuuxEoZimrSd0VIaLzsEsZoq9mcVh3yq1FN0ts1-HnLmF5HvmAbLU-dakBUqbyVD8TMg7N54W160XvM0aGag7hJCk0WMW1b90cL3cicAHS5i2BW_0JQO2QhNvx2OiISjb6S-KrjyuLFsBK7AZVZqoS0MPET0uacToQtG-dOy_CZO43-gqeYXyY0D436I4FO4ziM5AwoUC6sO8z7vewS2k-vLF81EdFvYe0WPgUHtkWtlRJ8dU75qm3O_xyUs4ihuP47EHBIO_-ovVCqur-IOMy24DtGnFZt5O46e22Tyb84IGphp-DO_X67xBdpPujgjRXRgOi5QcYIQsqd97toCK46kIDDRRRt1IkLd8Q8EbVt8" > 
+                                       <a  href="https://drive.google.com/file/d/1-toKPHPI55iJpYDbXcU2h5rK__lVC5lV/view?usp=sharing" > 
                                            {darkMode? <MagicalButton align="left">Download</MagicalButton> : <Mdark  align="left">Download</Mdark>}  
                                           </a>&nbsp;&nbsp; </Card.Text>
  
@@ -52,7 +52,7 @@ const Skills = ({ darkMode }) => {
                                      <div class={darkMode ? "lead" : "lead"}>GIMO 2021 Day 2</div>      <br></br>       
                                     <Card.Text align="left">
                                                  
-                                       <a  href="https://mail-attachment.googleusercontent.com/attachment/u/1/?ui=2&ik=e3eea35a15&attid=0.2&permmsgid=msg-f:1701998537667956915&th=179eb6a705eed0b3&view=att&disp=inline&realattid=f_kpnywooe1&saddbat=ANGjdJ_OVKr4zFs9u0T86qRcBHEFcYhORpO0IMV1qPGxPaFIkAyyKF3hAa7z1SJzYSa-sFF4y7LplzBEIbU_7aPaFvS7RZqagnqLKfBKyWwxJsQ_NNBWRbhvZvj3S2uXWPCB4zZ9e-GtDGJo0bBf_H3m3HQvybjwJTOydDX2j7mYgqCg6osmV-6sa7d-S16gTzIuODoFKW6M_AUZ5-fxDMm63R8B4vE0g57yp2YM6uaJl35A4Y9OxSqoW8JpMj1ogVFKHRdho4ZtUEsh_7jkBTx-LxTnD5CoSqtE880QFo0kRRdQDTWzJhWGAtIE9rjAEhHMZHXvRRToEwvBXvDiTAE6ZLjuzQZMr-j9X8eOpxlAbv7cqcs2ur4SvpiDUlJ1wSHnAxjDksTt8zfTCF0LHtsVjWLTQf4-1O9KuUxry50UbBfw291vevF4uyFdHth_cDRFaXP-P4OXkIbC8DwvYOJCUkpQUmTB78uABXFhzmvSAbH3xIF3Ckgedy-LRtJysAfClp6M80amhqOAewv5BuJSqqw_AQ94k6SI2luDDRLOtckaqUheO6Wji9-dvhMrzt10_y-_oaD_MFOA4aSIcu6oLf5D7hT9rq7BRgjywbMPH2rP_ZLl5MoMwjDs3HsbCnt7a2IveFq9ydISJlGoB2c6HQl1CMIqcJ_RPEXgisGXENOaDNmwaQUpDmrcuG4" > 
+                                       <a  href="https://drive.google.com/file/d/1-MLIbRM3-1-bEUAvKSMCVkkCghXyVTzq/view?usp=sharing" > 
                                            {darkMode? <MagicalButton align="left">Download</MagicalButton> : <Mdark  align="left">Download</Mdark>}  
                                           </a>&nbsp;&nbsp; </Card.Text>
  
@@ -88,7 +88,7 @@ const Skills = ({ darkMode }) => {
                                      <div class={darkMode ? "lead" : "lead"}>MMO 2021 Day 1</div>      <br></br>       
                                     <Card.Text align="left">
                                                  
-                                       <a  href="https://mail-attachment.googleusercontent.com/attachment/u/1/?ui=2&ik=e3eea35a15&attid=0.1&permmsgid=msg-a:r-8450346915127056636&th=17e91a1c6de88366&view=att&disp=inline&realattid=f_kyu7p8dj0&saddbat=ANGjdJ9PnIwsr_1vZpwRSW6ppsnKndK4r3jsls0lCaWfKn9PSw0Ed6q5XolGlQ0GvV1ddPYCdJnPmUxtb4_PRF4PHLD453XNS4_P6GlecVEnu6EGRY35zhtBSny4VHrn8nG9lgYo9IX5Rce3YpIPLTwtr6ZcU1pOvgWkXIrI12xKoIFr86VwV7DDz0miMX7wniunKWelinpXsPMQ5c3dkpy9PjgLuZx8YoGor3gQgBwgJZKy_cA7MB-jsEYDNI96FL-81C2e2F-_6VHzB8bap9hL_z_VmhW5EsNYeL8d5PFYZqu281WUbQDPdllJQXga8BVQ5ps_Seez5K1DoUwoW4QsMU2ARVym60SKqtFSw9jsFuJDHvdWaqP7dbjsh7Zs4MOKfQHWeQLc_6Lus51rZmG1SYSBzp6MY3RBFnrCazwhO7EOMPtwCaH4eOThQJJpqyL8NU7qYfnc13uUU7fSzFR-OgQn_ZShcktVHYsfp80HPb-l6SLLTsTpgJiB2FsT7eYBNibI7lv0tmQEgl8Kecs1axIyM059hj6EfbE0ru7r-pg_IJ_w1EGUjdZcQXSHVCbCQqV7tNUrqLNoKfGmIeloh5-EbJCSzoYvarI47ZGknBVbmCQCSHMKuRt_hKJG2b1bl16WQezfPLtL2aA4dxQFHOUD9GYeNTPjbDP8EwUSnVdYIwquhD9B2X0bBa4" > 
+                                       <a  href="https://drive.google.com/file/d/1o8fNNSPig1tpkdqrrRPVVnkoFhUx_M3r/view?usp=sharing" > 
                                            {darkMode? <MagicalButton align="left">Download</MagicalButton> : <Mdark  align="left">Download</Mdark>}  
                                           </a>&nbsp;&nbsp; </Card.Text>
  
@@ -111,7 +111,7 @@ const Skills = ({ darkMode }) => {
                                      <div class={darkMode ? "lead" : "lead"}>MMO 2021 Day 2</div>      <br></br>       
                                     <Card.Text align="left">
                                                  
-                                       <a  href="https://mail-attachment.googleusercontent.com/attachment/u/1/?ui=2&ik=e3eea35a15&attid=0.2&permmsgid=msg-a:r-8450346915127056636&th=17e91a1c6de88366&view=att&disp=inline&realattid=f_kyu7p9rv1&saddbat=ANGjdJ-6WNepCgNKS14fIe339-01v6yY19pFujnwEcEdstKuOBbhoZYOO1-2GXkABg5YV9k1N7_5lK0qYO78sgwdKAy0_Ga-CyomyJkdh1V2nkvSSLWbzhLUHgPRQAq3ZuTRLJh68GZ8q8YoGfUADVg0xJjIUiHS8B78txogp-9to-MMFRhKSV97aWabc1lM0u5c0boYSw1yPE-uBngB3cZKe7IPf1w-qLd6Px91moOa8Ur-loYkrEvqtzWzOfuv4MayIjCHRFPO2Vbc3lTOos1xgJu3hmc-SDbzpz2pOj66NDFueKDf0MktykAB_Rs9kO5nqeaK3Q46i6UPa59Wl7_y5S4Ks-W-jl1ZvRoS_QX1zbkLUBjEJh21GPl1XT7EyT56pA9pJjQVFqXkk82wp2JUS-NzFW_Ib2pxaNC4WrSKKmD46zTki8mUbPlUUL0hJQd3ZrDrzlBCsu7Xf05m76sxV2lBYNP_h6CymohyKZwpghIS94hZKZqfo76S9ti9zW41_WB73tlx4IhHKyo7S5BaMBNPanfw-JsHuF8fjl8tMwn3J_83wANIEJ2UOsqWPxbNbMGSfxOSrCDHl7lkDJWCPj_Xh34olQrIs5Z3sgYVVp0V1fpFtONZX4mSQnmZYGYMy5Jt1Z45iIGV2YWJrQnkF3YYp5Ac2Y7scEfdBENsUchw9lboxjBpi9ItK34" > 
+                                       <a  href="https://drive.google.com/file/d/1hKl11P_gYYW5UHS1IZTb4h3FqrNL5h4l/view?usp=sharing" > 
                                            {darkMode? <MagicalButton align="left">Download</MagicalButton> : <Mdark  align="left">Download</Mdark>}  
                                           </a>&nbsp;&nbsp; </Card.Text>
  
@@ -170,7 +170,7 @@ const Skills = ({ darkMode }) => {
                                      <div class={darkMode ? "lead" : "lead"}>OIMC 2021 Day 1</div>      <br></br>       
                                     <Card.Text align="left">
                                                  
-                                       <a  href="https://mail-attachment.googleusercontent.com/attachment/u/1/?ui=2&ik=e3eea35a15&attid=0.1&permmsgid=msg-f:1722943964661818565&th=17e92068459224c5&view=att&disp=inline&saddbat=ANGjdJ-iDkolHa7po4foAFJnji7Av_xCerfaO4qKGTU_zyc3A4PE4TpCCAirjYME6bfsSEjzaLiSxFcZBAjYRutJqIQv1S6yz756rLgxL_ebwjDyi_y-iR_UkkUpR3CTczHG2brBuvs--2RRalt5kk_mv1uGxry1Tr0HNRnZDYCrnza7twsCu6mYm6b89P_d-muPcJ1temz-vD1Q15rlj0unc8DGXAT4Fi1XX9Uy-0z0zGZMxfC70ZoXBc-rP6jxNJ8M6s_P5gFRsdhtQBLaZAexHtxYojUL5FAdgYm-nE-UsiaA6jsF3RpafGAwCUNOo9E796GnVBcIxcoPqrBHNxOMtQwDO4dR9yciSWF_9EfkuIVZtI4Oe6AOfhM5no-zQc1lvCq94K81BLOA6qomo6ChdzI-_eKDB58rRC7mc4FVtvI7IVY2MRvJ6d4SPFv4RKtrQAMfK1mJq17s2qB0aNnNaOeT616DiBOetunCdFQ1lWJWKln8e1jkXt_yjXkz_WrbLNl0VTip0ipyQVshQI-TRuowDfOrIjbwpbG9likk4jwC5EdVmWgyJ2gTtr9ftzoV-O1dzzyu-_Lqo-kIAGek6X1pRgnQ94pzvlRQqbQI9j9bAu39NB7wJZauYos1vKQJEwk7u3Zl2ywg7qdUM5qk_N0Ct8zM0eqt3p9t5ThlKjzJLIscAkjHx1gSRBA" > 
+                                       <a  href="https://drive.google.com/file/d/1DqiEKW7KlHyJgAxcW-0C0uNo9G9VJlR_/view?usp=sharing" > 
                                            {darkMode? <MagicalButton align="left">Download</MagicalButton> : <Mdark  align="left">Download</Mdark>}  
                                           </a>&nbsp;&nbsp; </Card.Text>
  
@@ -193,7 +193,7 @@ const Skills = ({ darkMode }) => {
                                      <div class={darkMode ? "lead" : "lead"}>OIMC 2021 Day 2</div>      <br></br>       
                                     <Card.Text align="left">
                                                  
-                                       <a  href="https://mail-attachment.googleusercontent.com/attachment/u/1/?ui=2&ik=e3eea35a15&attid=0.2&permmsgid=msg-f:1722943964661818565&th=17e92068459224c5&view=att&disp=inline&saddbat=ANGjdJ9YedWmE4UVDJwAcNOtl12b_Gu4kpd_ZXjM9moEl69Zakp8MUhf-WherZM0N18UWvjbye8G-RzIGfzS3EsVGm8dHQdN4gMRvmGtumGchCdJ-Q4SBrq5p3Bz57NsXKl9ThhBvDwdGQ6ZFRz-viZ-vsAWBKwqoju4nwMPz8zRNNbYPmpVKNlHJBDxyEMxY9gOW8D8PZVeVZFcCjpD9g5ppGkH0tv87u5mdFzWbOTs_O_qX2i-Qp9M3aJU3Lietz9WAKwyCb4Op1wvz2ThO0uMUCcFMruyjhuSuqNno8d3-TYwfd4VMsG1zsu_ZenWLWAHfPhXUWw1XQBuhTP5Mp1jI7AF8vmcol_WX1b3ndqUV7f1-k2ogUpngLCnNx-uFbgQbvla2POfEzXSoYotOozUrMvdfZg06EmkZyAfINgm7UB2jQt5WntBVVNkGIsYT5QYl1va-lecIpI0ZvX5GwqKbk_MiYCNvbkrtabHNl4Z6kNtfrReBDbqaPIF0JJrNv424rwzrE8aJtfnDLlNBSHl8w9qTCLPVvDKFACTR2eICFrRuk3yBVzmp9anR9VCFJhlhGJTdabr1K4jycmlYPrv8DIt5-CwzzMkQO4m5v5FUrEzG_vfsA3ykmKzsJSYkjotGdr_5dpGiPmVdeBd0fB_dYs3v0nSaxGPG-3QL6TUkdpCcHZGjSYajYvkXI4" > 
+                                       <a  href="https://drive.google.com/file/d/168u6049jDfTznqrh8DNIVJqJ46Z2ffX2/view?usp=sharing" > 
                                            {darkMode? <MagicalButton align="left">Download</MagicalButton> : <Mdark  align="left">Download</Mdark>}  
                                           </a>&nbsp;&nbsp; </Card.Text>
  
@@ -223,7 +223,7 @@ const Skills = ({ darkMode }) => {
                                      <div class={darkMode ? "lead" : "lead"}>OJIMC 2021 Day 1</div>      <br></br>       
                                     <Card.Text align="left">
                                                  
-                                       <a  href="https://mail-attachment.googleusercontent.com/attachment/u/1/?ui=2&ik=e3eea35a15&attid=0.3&permmsgid=msg-f:1722943964661818565&th=17e92068459224c5&view=att&disp=inline&saddbat=ANGjdJ-gDNcfv0At4SFXwpim6eM47sujXe_cOAR1X-m-SX8p31nh0Dv_kS20hHMTiVOlyV6xcBYWVy9e3tZ7VVTDgSDVkUlC0F8y5lrENaCZN3JVflxx_0BePm3jsW8aq-bZ0S7_vGqqL86XOwvwv4dp166-tBwOoS_pzwZN1V13fGbaf3WcEiS-d0PQsKGeqNXt3omfyaIWACGumQ6LlErjJ4UeZf9V5DQCco46rzOVJqPpyBPnXs1L_t7P72_5789YcgTPiJRXlRPXnsrxgXqtoZ4yJwxTQvhnakC_DyDxCKaU7OKQpXynp-cCgOCr7FX4OcwIQ-fuQ3Hss7oQdraCwZB_WWk6kZ6D0rmZMdoEI-_-N2-SbodVE5hwI1GsXUUGO3N0SHBLesFiJSh1JXWjW-E4GJxgCQAwwQNLSlat2Kr0_4vom5Zs0BrF978DmGE61Qy20O9nV3hXl6helCGYHQ-6R-eUtvKc5oQg9QUuq1is2IzGuAyRstnKVh9qlj9dJTP5CL4arP0KVkePOQhkspt5c2kLF9Be12YDtxG-iIwNdEzR_n9wv2iUuiO58kRbo2oPKnLQnB_P_JAk2elSOD_CWuje9KcvEIrj6S5QHDqm6EZw3kzqVdjaeO9VFyPSI2038jyBM8fzqsfIv8PyegsaDxQbtR1eRfllV1ra7tB8zQYSjhxgNNg-A7k" > 
+                                       <a  href="https://drive.google.com/file/d/1uZAqZY0l_9J-8utqnQcSSFyrS99WVD_H/view?usp=sharing"> 
                                            {darkMode? <MagicalButton align="left">Download</MagicalButton> : <Mdark  align="left">Download</Mdark>}  
                                           </a>&nbsp;&nbsp; </Card.Text>
  
@@ -246,7 +246,7 @@ const Skills = ({ darkMode }) => {
                                      <div class={darkMode ? "lead" : "lead"}>OJIMC 2021 Day 2</div>      <br></br>       
                                     <Card.Text align="left">
                                                  
-                                       <a  href="https://mail-attachment.googleusercontent.com/attachment/u/1/?ui=2&ik=e3eea35a15&attid=0.4&permmsgid=msg-f:1722943964661818565&th=17e92068459224c5&view=att&disp=inline&saddbat=ANGjdJ_7Ar_GEW1yP_1B-Y6WZNjwRnfB_uocmHa-5KiqWALu6G4w0ZgqExblEeEMzAZ3XiOS8Aqa2-QO0kU07O0n6XNW-wdJ7HQUuIwqrF5E7nPYF7-aHN9USpawCOqTPBnB8lDmWloU91Wf4yATQgGR3j8bp7TDCxGTXBYSOlP636Px4n7BCbIRdsQDywpbdC5N-QjYJ1VkEyU1PIWqNQoOFhxviW5xHaHeTe8kUIEv5nJF5JKGXmaBNHzBbZ_TM0SNpImqJ_d7WOoyv-46giZiq2GmUNUSJA287B0bFv1fftJMT73z8Hd5V8tqFKz_Rj60vRcky5UmjkWPftGUnVj6aqjRcLY665i0vscCrEDPUTX3s6rAeoFwvHv_Pqc5COmKck-abTY8L4LawEP1N0L_bdfWTAi2NQXVdPottwMVzsrI1aEp2T5L05-WzVccmCX3mhYLN-e0bunozuo1gJFIFu5GyxTCJIGyF4apZ6R-f5dz68QRbyZkCsOvc8ngaT3ATVhgkBAQ2DAeAD6N6Wrpab8Ulwvs6QTRv5M0rhGzucrufL12g02zPUDH3OCo0xF5KmWQJwjbYm1ClMeADy5XAl-8oHmMhR04YOy5XLuSnkeOkGjpCxPotpzCF-IOM6Txiok3psFGh-5-Xow_JL9DY2XDFW7WGu3-N3xDY7i6RC6UDkNvIEIwAo5DCOohttps://mail-attachment.googleusercontent.com/attachment/u/1/?ui=2&ik=e3eea35a15&attid=0.2&permmsgid=msg-f:1722943964661818565&th=17e92068459224c5&view=att&disp=inline&saddbat=ANGjdJ9YedWmE4UVDJwAcNOtl12b_Gu4kpd_ZXjM9moEl69Zakp8MUhf-WherZM0N18UWvjbye8G-RzIGfzS3EsVGm8dHQdN4gMRvmGtumGchCdJ-Q4SBrq5p3Bz57NsXKl9ThhBvDwdGQ6ZFRz-viZ-vsAWBKwqoju4nwMPz8zRNNbYPmpVKNlHJBDxyEMxY9gOW8D8PZVeVZFcCjpD9g5ppGkH0tv87u5mdFzWbOTs_O_qX2i-Qp9M3aJU3Lietz9WAKwyCb4Op1wvz2ThO0uMUCcFMruyjhuSuqNno8d3-TYwfd4VMsG1zsu_ZenWLWAHfPhXUWw1XQBuhTP5Mp1jI7AF8vmcol_WX1b3ndqUV7f1-k2ogUpngLCnNx-uFbgQbvla2POfEzXSoYotOozUrMvdfZg06EmkZyAfINgm7UB2jQt5WntBVVNkGIsYT5QYl1va-lecIpI0ZvX5GwqKbk_MiYCNvbkrtabHNl4Z6kNtfrReBDbqaPIF0JJrNv424rwzrE8aJtfnDLlNBSHl8w9qTCLPVvDKFACTR2eICFrRuk3yBVzmp9anR9VCFJhlhGJTdabr1K4jycmlYPrv8DIt5-CwzzMkQO4m5v5FUrEzG_vfsA3ykmKzsJSYkjotGdr_5dpGiPmVdeBd0fB_dYs3v0nSaxGPG-3QL6TUkdpCcHZGjSYajYvkXI4" > 
+                                       <a  href="https://drive.google.com/file/d/1GySL9n5Utn-0yd_J9L1WmUUGdJqQ6xzC/view?usp=sharing" > 
                                            {darkMode? <MagicalButton align="left">Download</MagicalButton> : <Mdark  align="left">Download</Mdark>}  
                                           </a>&nbsp;&nbsp; </Card.Text>
  
