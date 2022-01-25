dbfbea831f7663fdc579f56b23a88f4667c0557f
gaussiancurvolympiads <79222966+gaussiancurvolympiads@users.noreply.github.com>
2022-01-25 19:58:50 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -18,16 +18,59 @@ const Skills = ({ darkMode }) => {
             <Slide>
                 <Container>
              <br></br>
-                    <h4 className="lead" align="center"><b>Past Year Papers and Sample Questions</b></h4>
+                    <h4 className="lead" align="center"><b>MMO</b></h4>
                     <br></br>
                     <Row>
-                      <Col lg={6}>
+                   
+                      <Col lg={4}>
                      <Card className="skill-nbcard">
             
                                 <Card.Body>
-                                     <div class={darkMode ? "lead" : "lead"}>MMO 2020 Day 1</div>         
+                                     <div class={darkMode ? "lead" : "lead"}>MMO 2021 Day 1</div>      <br></br>       
                                     <Card.Text align="left">
-                                                   
+                                                 
+                                       <a  href="https://mail-attachment.googleusercontent.com/attachment/u/1/?ui=2&ik=e3eea35a15&attid=0.1&permmsgid=msg-a:r-8450346915127056636&th=17e91a1c6de88366&view=att&disp=inline&realattid=f_kyu7p8dj0&saddbat=ANGjdJ9PnIwsr_1vZpwRSW6ppsnKndK4r3jsls0lCaWfKn9PSw0Ed6q5XolGlQ0GvV1ddPYCdJnPmUxtb4_PRF4PHLD453XNS4_P6GlecVEnu6EGRY35zhtBSny4VHrn8nG9lgYo9IX5Rce3YpIPLTwtr6ZcU1pOvgWkXIrI12xKoIFr86VwV7DDz0miMX7wniunKWelinpXsPMQ5c3dkpy9PjgLuZx8YoGor3gQgBwgJZKy_cA7MB-jsEYDNI96FL-81C2e2F-_6VHzB8bap9hL_z_VmhW5EsNYeL8d5PFYZqu281WUbQDPdllJQXga8BVQ5ps_Seez5K1DoUwoW4QsMU2ARVym60SKqtFSw9jsFuJDHvdWaqP7dbjsh7Zs4MOKfQHWeQLc_6Lus51rZmG1SYSBzp6MY3RBFnrCazwhO7EOMPtwCaH4eOThQJJpqyL8NU7qYfnc13uUU7fSzFR-OgQn_ZShcktVHYsfp80HPb-l6SLLTsTpgJiB2FsT7eYBNibI7lv0tmQEgl8Kecs1axIyM059hj6EfbE0ru7r-pg_IJ_w1EGUjdZcQXSHVCbCQqV7tNUrqLNoKfGmIeloh5-EbJCSzoYvarI47ZGknBVbmCQCSHMKuRt_hKJG2b1bl16WQezfPLtL2aA4dxQFHOUD9GYeNTPjbDP8EwUSnVdYIwquhD9B2X0bBa4" > 
+                                           {darkMode? <MagicalButton align="left">Download</MagicalButton> : <Mdark  align="left">Download</Mdark>}  
+                                          </a>&nbsp;&nbsp; </Card.Text>
+ 
+                                </Card.Body>
+
+                            </Card></Col>
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      <Col lg={4}>
+                     <Card className="skill-nbcard">
+            
+                                <Card.Body>
+                                     <div class={darkMode ? "lead" : "lead"}>MMO 2021 Day 2</div>      <br></br>       
+                                    <Card.Text align="left">
+                                                 
+                                       <a  href="https://mail-attachment.googleusercontent.com/attachment/u/1/?ui=2&ik=e3eea35a15&attid=0.2&permmsgid=msg-a:r-8450346915127056636&th=17e91a1c6de88366&view=att&disp=inline&realattid=f_kyu7p9rv1&saddbat=ANGjdJ-6WNepCgNKS14fIe339-01v6yY19pFujnwEcEdstKuOBbhoZYOO1-2GXkABg5YV9k1N7_5lK0qYO78sgwdKAy0_Ga-CyomyJkdh1V2nkvSSLWbzhLUHgPRQAq3ZuTRLJh68GZ8q8YoGfUADVg0xJjIUiHS8B78txogp-9to-MMFRhKSV97aWabc1lM0u5c0boYSw1yPE-uBngB3cZKe7IPf1w-qLd6Px91moOa8Ur-loYkrEvqtzWzOfuv4MayIjCHRFPO2Vbc3lTOos1xgJu3hmc-SDbzpz2pOj66NDFueKDf0MktykAB_Rs9kO5nqeaK3Q46i6UPa59Wl7_y5S4Ks-W-jl1ZvRoS_QX1zbkLUBjEJh21GPl1XT7EyT56pA9pJjQVFqXkk82wp2JUS-NzFW_Ib2pxaNC4WrSKKmD46zTki8mUbPlUUL0hJQd3ZrDrzlBCsu7Xf05m76sxV2lBYNP_h6CymohyKZwpghIS94hZKZqfo76S9ti9zW41_WB73tlx4IhHKyo7S5BaMBNPanfw-JsHuF8fjl8tMwn3J_83wANIEJ2UOsqWPxbNbMGSfxOSrCDHl7lkDJWCPj_Xh34olQrIs5Z3sgYVVp0V1fpFtONZX4mSQnmZYGYMy5Jt1Z45iIGV2YWJrQnkF3YYp5Ac2Y7scEfdBENsUchw9lboxjBpi9ItK34" > 
+                                           {darkMode? <MagicalButton align="left">Download</MagicalButton> : <Mdark  align="left">Download</Mdark>}  
+                                          </a>&nbsp;&nbsp; </Card.Text>
+ 
+                                </Card.Body>
+
+                            </Card></Col>
+                      
+                      
+                      
+                      
+                      
+                      <Col lg={4}>
+                     <Card className="skill-nbcard">
+            
+                                <Card.Body>
+                                     <div class={darkMode ? "lead" : "lead"}>MMO 2020 Day 1</div>  MMO 2020 Day 1   <br></br>       
+                                    <Card.Text align="left">
+                                                 
                                        <a  href="https://aritra-12.github.io/pdfs/MMO%202020%20DAY%201.pdf" > 
                                            {darkMode? <MagicalButton align="left">Download</MagicalButton> : <Mdark  align="left">Download</Mdark>}  
                                           </a>&nbsp;&nbsp; </Card.Text>
@@ -35,13 +78,13 @@ const Skills = ({ darkMode }) => {
                                 </Card.Body>
 
                             </Card></Col>
- <Col lg={6}>
+ <Col lg={4}>
                             <Card className="skill-nbcard">
                             
                           
                                 <Card.Body >
                                       <div class={darkMode ? "lead" : "lead"}>MMO 2020 Day 2</div>
-                                      
+                                      <br></br>
                                       <Card.Text align="left">
                                              
   <a href="https://aritra-12.github.io/pdfs/MMO%202020%20DAY%202.pdf" >
